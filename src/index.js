96248704a50ee37ab83aa1c2c0dc5d6700509bf1
ImLostPlsHelp <107686662+ImLostPlsHelp@users.noreply.github.com>
2025-04-18 01:11:58 +0700

    Finished implementing REST on renderNotes

--- src/index.js
+++ src/index.js
@@ -7,22 +7,38 @@ import "./scripts/components/footer-bar.js";
 
 import "./scripts/components/note-list.js";
 import './scripts/components/note-item.js';
-import Notes from './scripts/notes.js';
+// import Notes from './scripts/notes.js';
 
 // import "./components/note-input.js";
 
 const noteList = document.querySelector('note-list');
 
-function renderNotes() {
-    const notes = Notes.getAll();
+async function renderNotes() {
     noteList.innerHTML = '';
+    
+    try {
+        const response = await fetch('https://notes-api.dicoding.dev/v2/notes');
+        const notes = await response.json();
 
-    notes.forEach(note => {
-        const noteItem = document.createElement('note-item');
-        noteItem.noteData = note;
+        if(notes.status === 'success') {
+            notes.data.forEach(note => {
+                const noteItem = document.createElement('note-item');
+                noteItem.noteData = {
+                    id: note.id,
+                    title: note.title,
+                    body: note.body,
+                    createdAt: note.createdAt,
+                    archived: note.archived
+                };
+                noteList.appendChild(noteItem);
+            });
+        } else {
+            noteList.innerHTML = '<p>Gagal memuat catatan.</p>';
+        }
+    } catch (error) {
+        noteList.innerHTML = '<p>Gagal memuat catatan.</p>';
 
-        noteList.appendChild(noteItem);
-    })
+    }
 }
 
 renderNotes();
